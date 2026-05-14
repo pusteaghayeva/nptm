1cc84ed20c887be044d4c6a9efa9bcd681652ae2
pusteaghayeva <puste.aghayeva@gmail.com>
2026-05-14 11:42:00 +0400

    edit day2

---
+++ src/js/private_cabinet.js
@@ -0,0 +1,11 @@
+function togglePassword(id) {
+    const input = document.getElementById(id);
+    const icon = document.getElementById(id + '-icon');
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.classList.replace('fa-eye-slash', 'fa-eye');
+    } else {
+        input.type = 'password';
+        icon.classList.replace('fa-eye', 'fa-eye-slash');
+    }
+}
